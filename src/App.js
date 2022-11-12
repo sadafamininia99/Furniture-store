aa5c0eef20ea7cdaf6e595fcc6f77e718f2ff2a5
SadafAmininiiiaa <s.amininia@tapsell.ir>
2022-11-12 11:14:45 +0330

    init first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+
+function App(){
+    return(
+        <div>
+            ejkkjre
+        </div>
+    );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+import cartReducer from "./ducks/cart";
+import productsReducer from "./ducks/products";
+import App from "./App";
+import productsData from "./data/products";
+import "bootstrap/dist/css/bootstrap.css";
