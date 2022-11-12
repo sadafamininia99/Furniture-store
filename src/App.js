8a2273a567e43cd511a7e82bd04098bf750f26ee
SadafAmininiiiaa <s.amininia@tapsell.ir>
2022-11-12 11:33:52 +0330

    config project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import "./App.css";
-
+import React from "react";
 
 function App(){
     return(
         <div>
-            ejkkjre
-        </div>
+            hi
+            </div>
     );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,3 +7,19 @@ import productsReducer from "./ducks/products";
 import App from "./App";
 import productsData from "./data/products";
 import "bootstrap/dist/css/bootstrap.css";
+
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    products: productsReducer,
+});
+
+let store = createStore(rootReducer, {
+    products: productsData, // initial store values
+});
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
