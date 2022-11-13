66b2204e6247823f94404b88d43413b4054b91a8
SadafAmininiiiaa <aminicomputer96@gmail.com>
2022-11-13 13:09:42 +0330

    the structure of the project

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ function App() {
     return (
         <div className="conatainer">
             <div className="row">
-                .<div className="col-md-12">
+                <div className="col-md-12">
                     <h1>
-                        kdkdkdkkkkkkkkkkkkkkkkkk
-                        shopping cart
+                        shopping store
                     </h1>
+                </div>
+                <div className="row">
+                    <div className="col-md-8">
+                        {/* //component here */}
+                    </div>
+                    <div className="col-md-4">
+                        {/* //compnent  */}
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
 export default App;
-//dwwwwwwwwwwwwwwww
-//some chanegessss"
+
